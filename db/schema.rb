234fb0bccad2cda9669336c17a5079c5e0ca344d
LY QUACH <lyllq.quach@gmail.com>
2021-10-15 17:33:59 +0200

    review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_195922) do
+ActiveRecord::Schema.define(version: 2021_10_15_153250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,17 @@ ActiveRecord::Schema.define(version: 2021_10_14_195922) do
     t.index ["user_id"], name: "index_passports_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "passport_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["passport_id"], name: "index_reviews_on_passport_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "transactions", force: :cascade do |t|
     t.date "transaction_date"
     t.date "rental_start"
@@ -84,6 +95,8 @@ ActiveRecord::Schema.define(version: 2021_10_14_195922) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "passports", "users"
+  add_foreign_key "reviews", "passports"
+  add_foreign_key "reviews", "users"
   add_foreign_key "transactions", "passports"
   add_foreign_key "transactions", "users"
 end
