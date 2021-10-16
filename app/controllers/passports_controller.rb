ea21bf7b817580d3f382abe1f9a554288804e0e9
JulienDbrt <juliendabert@outlook.com>
2021-10-16 12:17:13 +0200

    fixed cloudinary issues

--- app/controllers/passports_controller.rb
+++ app/controllers/passports_controller.rb
@@ -3,7 +3,7 @@ class PassportsController < ApplicationController
 
   def index
     if params[:brand].present?
-      @passports = Passport.where("brand_name ILIKE ?", "%#{params[:brand]}%").and(Passport.where("gender ILIKE ?", "%#{params[:gender]}%"))
+      @passports = Passport.where("brand_name ILIKE ?", "%#{params[:brand]}%").and(Passport.where("gender ILIKE ?", "%#{params[:gender]}%")).and(Passport.where("availability_date LIKE ?" , "%#{params[:availability_date]}%"))
     else
       @passports = Passport.all
     end
